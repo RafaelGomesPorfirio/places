c23b09bcc4758d26da516a86861e4af4b7c5d22b
Rafael <rafagomes475@gmail.com>
2023-01-14 12:09:53 -0300

    Models

--- database/migrations/2023_01_13_180922_create_places_table.php
+++ database/migrations/2023_01_13_180922_create_places_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('places', function (Blueprint $table) {
+        Schema::create('Places', function (Blueprint $table) {
             $table->id();
             
             $table->string('name');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('places');
+        Schema::dropIfExists('Places');
     }
 };
